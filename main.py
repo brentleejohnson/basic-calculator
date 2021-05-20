65292f19bcf6695b6d067797a7d58866294e6098
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-20 15:56:21 +0200

    Added main and other file

---
+++ main.py
@@ -0,0 +1,33 @@
+# Basic Calculator
+
+class Add:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        # sum = a + b (unnecessary)
+        return a + b
+
+
+class Subtract:
+    def __init__(self):
+        pass
+
+    def subtract(self, a, b):
+        return a - b
+
+
+class Divide:
+    def __init__(self):
+        pass
+
+    def divide(self, a, b):
+        return a / b
+
+
+class Multiply:
+    def __init__(self):
+        pass
+
+    def multiply(self, a, b):
+        return a * b
